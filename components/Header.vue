5db2b6db8670626cbfc562d97ba417bf762d7c9d
Yulia <warwarachernih@gmail.com>
2025-06-05 22:59:13 +1000

    4 task(header)

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,31 @@
+<script setup>
+const phoneNumber = '+7 (800) 900-90-90';
+import HomeIcon from '~/assets/logo.svg';
+import PhoneIcon from '~/assets/phone.svg';
+</script>
 <template>
-    <header>
-      <h1>Header</h1>
-    </header>
-  </template>
-  
-  <style scoped lang="scss">
-  header {
-    background-color: #a7d9ed;
-    padding: 10px;
-    text-align: center;
-    h1 {
-      font-size: 2em;
-      margin-bottom: 10px;
-      text-align: center;
-    }
-  }
-  </style>
+  <header class="header">
+    <div class="header__container">
+      <div class="header__logo">
+        <img :src="HomeIcon" alt="Home" class="header__icon">zagdom</div>
+      
+      <nav class="header__nav">
+        <a href="#" class="header__link">Реализованные проекты</a>
+        <a href="#" class="header__link">Новости</a>
+        <a href="#" class="header__link">Контакты</a>
+      </nav>
+      
+      <div class="header__contacts">
+        <a :href="`tel:${phoneNumber.replace(/\D/g, '')}`" class="header__phone">
+          <img :src="PhoneIcon" alt="Phone" class="header__icon">
+          {{ phoneNumber }}
+        </a>
+        <button class="header__button">Оставить заявку</button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+@use "~/styles/header";
+</style>
